627ee63dff16fcfe1f6033fe88630e12035ca610
Schlemmer Jannik Lukas <js329@hdm-stuttgart.de>
2018-05-05 12:16:57 +0200

    labeled apps and write into a dictionary

--- task1_MobileUsers/Code/FeatureExtracction.py
+++ task1_MobileUsers/Code/FeatureExtracction.py
@@ -20,18 +20,24 @@ from CSVtoSQLite import disk_engine
 import pandas as pd
 import numpy as np
 
-events_df = pd.read_sql("SELECT device_id as device, event_id as event "
-                        "FROM events "
-                        "WHERE device_id = '9206538029661406976'",
-                        disk_engine)
-# get all installed Apps during all events of the device
-eventIDs = tuple(events_df.event.values)
-apps = pd.read_sql("SELECT DISTINCT app_id as app FROM app_events WHERE event_id in " + str(eventIDs), disk_engine)
 
-# get app_labels for each installed app and store them into a dictionary
-appLabels = {}
-apps['app_Labels'] = np.nan
-for index, row in apps.iterrows():
-    app_labels = pd.read_sql("SELECT label_id as appLabel FROM app_labels WHERE app_id = "
-                             + str(apps['app'][index]), disk_engine)
-    appLabels[apps['app'][index]] = set(app_labels['appLabel'].values)
+def labelAppsPerEvent():
+    events_df = pd.read_sql("SELECT device_id as device, event_id as event "
+                            "FROM events "
+                            "WHERE device_id = '9206538029661406976'",
+                            disk_engine)
+    # get all installed Apps during all events of the device
+    eventIDs = tuple(events_df.event.values)
+    apps = pd.read_sql("SELECT DISTINCT app_id as app FROM app_events WHERE event_id in " + str(eventIDs), disk_engine)
+
+
+def labelAllApps():
+    allAppsLabeled = pd.read_sql("SELECT app_id as app, label_id FROM app_labels ORDER BY app", disk_engine)
+    appDict = {}
+    for app in set(allAppsLabeled['app']):
+        appDict[app] = list(allAppsLabeled.loc[allAppsLabeled['app'] == app]['label_id'])
+
+    return appDict
+
+
+labelAllApps()
