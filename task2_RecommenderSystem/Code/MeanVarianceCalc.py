8024b8c2f9c81ff8c30bda0be2e3d41a8097ba49
Schlemmer Jannik Lukas <js329@hdm-stuttgart.de>
2018-05-07 10:34:46 +0200

    started work on assignment 'RecommenderSystems'

---
+++ task2_RecommenderSystem/Code/MeanVarianceCalc.py
@@ -0,0 +1,25 @@
+def calculateMean(vector):
+    summe = 0
+    for element in vector:
+        summe += element
+    result = summe / len(vector)
+    return result
+
+
+def variance(vector):
+    mean = calculateMean(vector)
+    summe = 0
+    for element in vector:
+        summe += (element - mean) ** 2
+    result = summe / len(vector)
+    return result
+
+
+a = [1, 2, 3, 4, 5, 6]
+b = [3, 3, 5, 6, 7, 8]
+
+
+print("Mean a:", calculateMean(a))
+print("Mean b:", calculateMean(b))
+print("Variance a:", variance(a))
+print("Variance b:", variance(b))
