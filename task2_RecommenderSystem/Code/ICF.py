6c438dfa4a29bdee3c88d1982f0e7a170b8c9631
Schlemmer Jannik Lukas <js329@hdm-stuttgart.de>
2018-05-14 11:14:33 +0200

    little changes

--- task2_RecommenderSystem/Code/ICF.py
+++ task2_RecommenderSystem/Code/ICF.py
@@ -7,30 +7,36 @@ NR.1
 #3 Using .iteritems to iterate over key, value in dic
    .iteritems not work --> change to .items
 '''
+
+
 def transCritics(critics):
     movies = {}
 
-    #1 #3
+    # 1 #3
     for critics, details in critics.items():
         for title, rating in details.items():
-            #2
+            # 2
             if title not in movies:
                 movies[title] = {}
             movies[title][critics] = rating
 
     return movies
 
+
 '''
 NR.2
 add transCritics to Dic
 '''
-def  calculateSimilarItems(prefs, similarity):
+
+
+def calculateSimilarItems(prefs, similarity):
     simItems = {}
 
     for title in prefs:
-        simItems[title] = topMatches(prefs,title,similarity)
+        simItems[title] = topMatches(prefs, title, similarity)
 
     return simItems
+
 
 '''
 NR.3
@@ -40,8 +46,10 @@ calculation product recommendation
                        --> sumSimilarities might equal 0
 #3 insert weighted recommendation into sorted result array
 '''
-def getRecommendedItems(prefs,person,similarity):
-    simItem = calculateSimilarItems(prefs,similarity)
+
+
+def getRecommendedItems(prefs, person, similarity):
+    simItem = calculateSimilarItems(prefs, similarity)
     result = []
 
     notboughtMovies = []
@@ -54,29 +62,30 @@ def getRecommendedItems(prefs,person,similarity):
         sumSimilarities = 0
 
         for purchasedMovie, rating in critics[person].iteritems():
-            #1
+            # 1
             for item in simItem[notboughtMovie]:
-                #2
+                # 2
                 if item[0] == purchasedMovie:
                     if item[1] > 0:
                         sumRecommendation += rating * item[1]
                         sumSimilarities += item[1]
-    #2
+    # 2
     recommandation = sumRecommendation / sumSimilarities if sumSimilarities != 0 else 0
 
-    #3
+    # 3
     if len(result) == 0:
-        result.append([notboughtMovie,recommandation])
+        result.append([notboughtMovie, recommandation])
     else:
         index = 0
         for i, value in enumerate(result):
             if value[1] > recommandation:
-                index = i+1
+                index = i + 1
             else:
                 break
-        result.insert(index, [notboughtMovie,recommandation])
+        result.insert(index, [notboughtMovie, recommandation])
 
     return result
+
 
 transcitics = transCritics(critics)
 
@@ -85,11 +94,11 @@ NR.4
 Recommendation for Toby
 '''
 
+
 def run_icf():
-    print('-'*50, '\n', 'Item based collaborative filtering \n', '-'*50)
+    print('-' * 50, '\n', 'Item based collaborative filtering \n', '-' * 50)
     tc = transCritics(critics)
     person = 'Toby'
-
 
     print('Recommendations for %s ( Euclidean Distance): \n' % (person),
           getRecommendedItems(tc, person, sim_euclid))
@@ -97,8 +106,7 @@ def run_icf():
     print('Recommendations for %s ( Pearson Distance): \n' % (person),
           getRecommendedItems(tc, person, sim_pearson))
 
-    print('-'*50)
+    print('-' * 50)
+
 
 run_icf()
-
-
