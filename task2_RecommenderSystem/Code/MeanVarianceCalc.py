6a886790ff3af2c552bd7d03ac9c90c2d5b9994c
Schlemmer Jannik Lukas <js329@hdm-stuttgart.de>
2018-05-21 17:29:24 +0200

    added todo.txt

--- task2_RecommenderSystem/Code/MeanVarianceCalc.py
+++ task2_RecommenderSystem/Code/MeanVarianceCalc.py
@@ -2,7 +2,7 @@ def calculateMean(vector):
     summe = 0
     for element in vector:
         summe += element
-    result = summe / len(vector)
+    result = float(summe) / float(len(vector))
     return result
 
 
@@ -11,7 +11,7 @@ def variance(vector):
     summe = 0
     for element in vector:
         summe += (element - mean) ** 2
-    result = summe / len(vector)
+    result = float(summe) / float(len(vector))
     return result
 
 
@@ -19,10 +19,10 @@ a = [1, 2, 3, 4, 5, 6]
 b = [3, 3, 5, 6, 7, 8]
 
 
-print("Mean a:", calculateMean(a))
-print("Mean b:", calculateMean(b))
-print("Variance a:", variance(a))
-print("Variance b:", variance(b))
+print "Mean a:", calculateMean(a)
+print "Mean b:", calculateMean(b)
+print "Variance a:", variance(a)
+print "Variance b:", variance(b)
 
 
 
